5915a2f37ced46303f7e1604af82a19dfd484a51
mini mocha <minimocha.figure-8.co>
2020-01-10 13:31:16 -0500

    feat: add story pages and style

---
+++ stories/index.tsx
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import { storiesOf } from '@storybook/react';
+
+//* Import pages
+// import ButtonPage from './pages/ButtonPage';
+
+//* Add stories
+// storiesOf('General', module)
+//   .add('Button', () => <ButtonPage />)
