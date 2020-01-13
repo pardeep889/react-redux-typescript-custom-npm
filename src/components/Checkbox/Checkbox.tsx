28796f9f79429515483e3e4d905ba903bc8fe140
mini mocha <minimocha.figure-8.co>
2020-01-13 17:19:06 -0500

    feat: add checkbox component

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import Checkbox, {  } from 'antd/es/checkbox';
+import Checkbox, { CheckboxProps as AntCheckboxProps } from 'antd/es/checkbox';
+import { Text } from '../index';
+
+import './less/checkbox.less';
+
+interface CheckboxProps extends AntCheckboxProps {}
+
+function CheckboxWrapper (props: CheckboxProps){
+  
+  const checkboxClass = `checkbox`
+
+  return (
+    <Checkbox className={checkboxClass} {...props}>
+      <Text className="checkbox-text" >{props.children}</Text>
+    </Checkbox>
+  )
+};
+
+export default CheckboxWrapper;
--- src/components/index.ts
+++ src/components/index.ts
@@ -7,3 +7,4 @@ export { default as Button } from './Button/Button';
 export { default as Title } from './Typography/Title';
 export { default as Text } from './Typography/Text';
 export { default as Input } from './Input/Input';
+export { default as Checkbox } from './Checkbox/Checkbox';
