fbc4de76ff677b61645461469f90e5c62b06166f
mini mocha <minimocha.figure-8.co>
2020-01-14 15:18:24 -0500

    feat: add button component and styling

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,7 @@
 import '@style/index.less';
 
 //* All Components to be exported here
-// Example: 
-// export { default as Button } from './Button/Button';
 
+export { default as Button } from './Button/Button';
 export { default as Title } from './Typography/Title';
 export { default as Text } from './Typography/Text';
