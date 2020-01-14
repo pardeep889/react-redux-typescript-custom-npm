b7bb058c70d8cb80f5493512b623ff3c10bc52ee
mini mocha <minimocha.figure-8.co>
2020-01-14 15:51:27 -0500

    feat: add text input field story

--- stories/index.tsx
+++ stories/index.tsx
@@ -3,8 +3,11 @@ import { storiesOf } from '@storybook/react';
 
 //* Import pages
 import TypographyPage from './pages/TypographyPage';
+import InputPage from './pages/InputPage';
+
 import './assets/style/index.less';
 
 //* Add stories
 storiesOf('General', module)
+.add('Input', () => <InputPage />)
 .add('Typography', () => <TypographyPage />);
