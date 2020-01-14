7d713c249cd10966a0c64ed49756d2b40233a930
mini mocha <minimocha.figure-8.co>
2020-01-14 15:18:40 -0500

    feat: add button story

--- stories/index.tsx
+++ stories/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 //* Import pages
+import ButtonPage from './pages/ButtonPage';
 import TypographyPage from './pages/TypographyPage';
 import './assets/style/index.less';
 
 //* Add stories
 storiesOf('General', module)
+.add('Button', () => <ButtonPage />)
 .add('Typography', () => <TypographyPage />);
