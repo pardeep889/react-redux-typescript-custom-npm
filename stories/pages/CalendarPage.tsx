d772186626d489917b2eca7b6bf7f3aa8ffc9c5f
mini mocha <minimocha.figure-8.co>
2020-01-17 16:35:54 -0500

    feat: update story to add date cell

--- stories/pages/CalendarPage.tsx
+++ stories/pages/CalendarPage.tsx
@@ -5,15 +5,23 @@ import React from 'react';
 
 //COMPONENT
 import { Calendar }  from '@components/index';
+import DateCell from '@components/Calendar/DateCell';
+
 
 const CalendarPage = () => {
   return (
     <div>
       <h3 className="story-title">Calendar</h3>
       <section className="m20">
-        <h4 className="mb20">Date Picker</h4>
+        <h4 className="mb20">Date Cell</h4>
         <div className="m20">
-          <Calendar />
+          <DateCell>6</DateCell>
+        </div>
+      </section>
+      <section className="m20">
+        <h4 className="mb20">Calendar</h4>
+        <div className="m20">
+          <Calendar resetDay={2} soldOutDates={["2020-02-02", "2020-02-07"]} />
         </div>
       </section>
     </div>
