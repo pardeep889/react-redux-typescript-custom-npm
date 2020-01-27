73728e099d622642de49f37548fe5dd83ffebb86
Sewina <minimocha@figure-8.co>
2020-01-27 16:21:42 -0500

    feat: add prop types

--- src/components/ToolTip/ToolTip.tsx
+++ src/components/ToolTip/ToolTip.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import Tooltip, { TooltipProps as AntTooltipProps } from 'antd/es/tooltip';
+import Tooltip, { AbstractTooltipProps as AntAbstractTooltipProps } from 'antd/es/tooltip';
 
 import './less/tool-tip.less';
 
-interface ToolTipProps {
+interface ToolTipProps extends AntAbstractTooltipProps {
 
 }
 
 const ToolTipWrapper = (props: ToolTipProps) => {
 	return (
-		<div></div>
+		<div>
+			
+		</div>
+		// <Tooltip {...props}>
+		// 	{props.children}
+		// </Tooltip>
 	)
 }
 
