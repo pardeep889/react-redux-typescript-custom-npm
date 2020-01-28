071c8bf5c6723912b7e912c54f2512e6c4808ffc
Sewina <minimocha@figure-8.co>
2020-01-28 17:04:09 -0500

    feat: add selector styling

--- src/components/Selector/Selector.tsx
+++ src/components/Selector/Selector.tsx
@@ -4,16 +4,47 @@ import Button, { ButtonProps as AntButtonProps } from 'antd/es/button';
 import './less/selector.less';
 
 //* Custom API Props for Button
-interface ButtonProps extends Omit<AntButtonProps, 'type'> {
+interface ButtonProps extends Omit<AntButtonProps, 'type' | 'icon'> {
 	type: "primary" | "secondary";
+	icon: JSX.Element;
 }
 
 function Selector(props: ButtonProps) {
+	const { value, icon, type } = props;
+	const newProps = buttonPropsUpdate(props);
+
+	let buttonText = value ? value : props.children;
+
+	const selectorClass = `selector-${type}`
 
 	return(
-		<div></div>
+	<Button {...newProps} className={selectorClass}>
+		<div className="selector-icon">{icon}</div>
+		{buttonText}
+	</Button>
 	)
 };
+
+const buttonPropsUpdate = (props: ButtonProps) => {
+	const { type } = props
+	let buttonType: "ghost" | "link" | "default" | "primary" | "dashed" | "danger" | undefined;
+
+	switch(type){
+		case "secondary":
+			buttonType = "default";
+			break;
+		default:
+			buttonType = "primary";
+	}
+
+	const newProps = {
+		...props,
+		type: buttonType,
+		icon: undefined,
+	}
+
+	return newProps;
+}
 
 // This export will be picked up in ./index.js
 export default Selector;
--- stories/pages/SelectorPage.tsx
+++ stories/pages/SelectorPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 //COMPONENT
 import { Selector } from '@components/index';
+import Guest from '@icon/Guest';
 
 const SelectorPage = () => {
   return (
@@ -9,8 +10,16 @@ const SelectorPage = () => {
       <h3 className="story-title">Selector Types</h3>
       <section>
         <h4 className="story-title">Primary</h4>
-        <div className="m20">
-          <Selector type="primary"/>
+        <div className="m20 story-background">
+          <div className="m20">
+            <h6 className="mb20">Normal</h6>
+            <Selector 
+              type="primary" 
+              icon={<Guest/>}
+            >
+              Choose City    
+            </Selector>
+          </div>
         </div>
       </section>
     </div>
