b3367dcc40f718ae92a5dc2d3d94e3f5310e9358
Sewina <minimocha@figure-8.co>
2020-01-30 17:28:44 -0500

    feat: add styling for titles

--- src/components/Typography/Title.tsx
+++ src/components/Typography/Title.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
-import './less/typography.less';
+import './less/title.less';
 
 interface TitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
   level: "h1" | "h2" | "h3" | "h4" | "h6" | "h7";
   disabled?: boolean | undefined;
+  color?: "default" | "cherry";
 }
 
 function Title(props: TitleProps) {
-  const { level, disabled } = props;
-  let titleDisabled = "";
+  const { level, disabled, color } = props;
 
   // Classes
-  disabled ? titleDisabled = "disabled" : ""
-  const titleClass = `title ${level} ${titleDisabled}`;
+  const titleDisabled = disabled ? "-disabled" : "";
+  const titleColor = color==="cherry" ? "title-color" : "";
+  const titleClass = disabled ? `title-${level}${titleDisabled}` : `title-${level} ${titleColor}`;
 
   return (
     <h1{...props} className={titleClass} >{props.children}</h1>
--- stories/pages/TypographyPage.tsx
+++ stories/pages/TypographyPage.tsx
@@ -13,17 +13,27 @@ const TypographyPage = () => {
       <section>
         <h4 className="story-title">Titles</h4>
         <div className="m20">
-          <Title level="h1"> H1 Sample Text </Title>
-        </div>
-        <div className="m20">
-          <Title level="h2"> H2 Sample Text </Title>
-        </div>
-        <div className="m20">
-          <Title level="h3"> H3 Sample Text </Title>
-        </div>
-        <div className="m20">
-          <Title level="h4"> H4 Sample Text </Title>
-          <Title level="h4" disabled > H4 Sample Text </Title>
+          <div className="m20">
+            <Title level="h1"> H1 Sample Text </Title>
+          </div>
+          <div className="m20">
+            <Title level="h2"> H2 Sample Text </Title>
+            <Title level="h2" color="cherry"> H2 Colored Text </Title>
+          </div>
+          <div className="m20">
+            <Title level="h3"> H3 Sample Text </Title>
+          </div>
+          <div className="m20">
+            <Title level="h4"> H4 Sample Text </Title>
+          </div>
+          <div className="m20">
+            <Title level="h6"> H6 Sample Text </Title>
+            <Title level="h6" disabled> H6 Disabled Text </Title>
+          </div>
+          <div className="m20">
+            <Title level="h7"> H7 Sample Text </Title>
+            <Title level="h7" color="cherry"> H7 Colored Text </Title>
+          </div>
         </div>
       </section>
       <section>
