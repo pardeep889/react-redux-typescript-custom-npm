942425808aae5bd886bdd4043d5ca47a1173fc85
Sewina <minimocha@figure-8.co>
2020-01-31 08:26:05 -0500

    feat: fix code to accommodate different text color

--- src/components/Typography/Title.tsx
+++ src/components/Typography/Title.tsx
@@ -5,6 +5,8 @@ import './less/title.less';
 interface TitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
   level: "h1" | "h2" | "h3" | "h4" | "h6" | "h7";
   disabled?: boolean | undefined;
+
+  // add more colors as needed to props and update title.less file
   color?: "default" | "cherry";
 }
 
@@ -13,7 +15,7 @@ function Title(props: TitleProps) {
 
   // Classes
   const titleDisabled = disabled ? "-disabled" : "";
-  const titleColor = color==="cherry" ? "title-color" : "";
+  const titleColor = color === "default" ? "": `color-${color}`;
   const titleClass = disabled ? `title-${level}${titleDisabled}` : `title-${level} ${titleColor}`;
 
   return (
