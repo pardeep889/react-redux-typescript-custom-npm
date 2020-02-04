92d03e0c6c5fb5d411a4c33035ea523ba8503637
Sewina <minimocha@figure-8.co>
2020-02-04 15:32:41 -0500

    feat: setup dropdown files

---
+++ src/components/DropDown/DropDown.tsx
@@ -0,0 +1,20 @@
+  
+import React from 'react';
+import Button, { ButtonProps as AntButtonProps } from 'antd/es/button';
+
+import './less/dropdown.less';
+
+//* Custom API Props for Button
+interface DropDownProps {
+
+}
+
+function DropDown(props: DropDownProps) {
+
+	return(
+		<div></div>
+	)
+};
+
+// This export will be picked up in ./index.js
+export default DropDown;
---
+++ stories/pages/DropDownPage.tsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+
+//STORYBOOK
+// import { action } from '@storybook/addon-actions';
+
+//COMPONENT
+import { DropDown } from '@components/index';
+
+const DropDownPage = () => {
+  return (
+    <div>
+      <h3 className="story-title">DropDown</h3>
+      <section>
+        <h4 className="story-title">Types</h4>
+        <div className="m20">
+          {/* <DropDown /> */}
+        </div>
+      </section>
+    </div>
+  )
+};
+
+export default DropDownPage;
