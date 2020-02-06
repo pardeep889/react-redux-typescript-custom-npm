cd2d20b7c89d243a4d352557e8fbdd1a13a78191
Sewina <minimocha@figure-8.co>
2020-02-06 11:28:27 -0500

    feat: add partial type

--- stories/pages/DropDownPage.tsx
+++ stories/pages/DropDownPage.tsx
@@ -6,19 +6,46 @@ import React from 'react';
 
 //COMPONENT
 import { DropDown } from '@components/index';
+import Guest from '@icon/Guest';
 
-const dropDownSample = {
-  
-}
+const guestIcon = <Guest />
+const dropDownSample = [
+  { 
+    prefixValue: "+1",
+    value: "value 1",
+    icon: guestIcon
+  },
+  { 
+    prefixValue: "+44",
+    value: "value 2",
+    icon: guestIcon
+  },
+  { 
+    prefixValue: "+010",
+    value: "value 3",
+    icon: guestIcon
+  },
+  { 
+    prefixValue: "+0021",
+    value: "value 4",
+    icon: guestIcon
+  },
+]
 
 const DropDownPage = () => {
   return (
     <div>
-      <h3 className="story-title">DropDown</h3>
+      <h3 className="story-title">Drop Down</h3>
       <section>
-        <h4 className="story-title">Types</h4>
+        <h4 className="story-title">Full Type</h4>
         <div className="m20">
-          <DropDown />
+          <DropDown type="full" placeholder="Large Size" optionList={dropDownSample}/>
+        </div>
+      </section>
+      <section>
+        <h4 className="story-title">Partial Type</h4>
+        <div className="m20">
+          <DropDown type="partial" defaultValue="value 1" optionList={dropDownSample}/>
         </div>
       </section>
     </div>
